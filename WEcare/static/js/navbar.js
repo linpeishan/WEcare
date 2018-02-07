da0ca8b3da65e1e6ec343dcb882de14acc9f0967
Johanna Jimeno <johannajimenoo@gmail.com>
2018-02-07 14:28:57 +0800

    Delete navbar.js

--- WEcare/static/js/navbar.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-$('.navbar .dropdown').hover(function () {
-        $(this).find('.dropdown-menu').first().stop(true, true).slideDown(150);
-    }, function () {
-        $(this).find('.dropdown-menu').first().stop(true, true).slideUp(105)
-    });
-});
